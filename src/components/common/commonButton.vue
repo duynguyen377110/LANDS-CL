0b2700385fcfdb7db7c16762a770b335d15eea57
duy366110 <duy366110@gmail.com>
2024-04-11 22:46:47 +0700

    #COnfig template signin and signup v-01.

---
+++ src/components/common/commonButton.vue
@@ -0,0 +1,29 @@
+<template>
+     <button
+        type="submit"
+        :class="{'full': full}"
+        class="btn-common btn btn-primary">{{title}}</button>
+</template>
+
+<script>
+
+    export default {
+        name: 'common-button',
+        props: {
+            title: String,
+            full: Boolean
+        },
+        components: { }
+    }
+</script>
+
+<style>
+    .btn-common {
+        background-color: var(--first-color);
+        font-size: 1.5rem;
+        padding: .5rem 0rem;
+    }
+    .full {
+        width: 100%;
+    }
+</style>
---
+++ src/hook/hook-validator.js
@@ -0,0 +1,97 @@
+
+const regexEmail = /^(([^<>()[\].,;:\s@"]+(.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+.)+[^<>()[\].,;:\s@"]{2,})$/i;
+const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/;
+const regexPhone = /^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/;
+
+const valid = (key, value) => {
+    if(key === 'required') {
+        if(!(value.trim() === 'DEFAULT') && value.trim().length) {
+
+            return {
+                status: true,
+                message: ''
+            }
+
+        } else {
+            return {
+                status: false,
+                message: 'Nội dung không được trống'
+            }
+        }
+    }
+
+    if(key === 'email') {
+        if(value.trim().toLowerCase().match(regexEmail)) {
+            return {
+                status: true,
+                message: ''
+            }
+        } else {
+            return {
+                status: false,
+                message: 'E-mail không hợp lệ'
+            }
+        }
+    }
+
+    if(key === 'password') {
+        if(regexPassword.test(value)) {
+            return {
+                status: true,
+                message: ''
+            }
+        } else {
+            return {
+                status: false,
+                message: 'Mật khẩu có độ dài tối thiểu 6 ký tự và các ký tự đặc biệt */#/@!..'
+            }
+        }
+    }
+
+    if(key === 'phone') {
+        if(regexPhone.test(value)) {
+            return {
+                status: true,
+                message: ''
+            }
+        } else {
+            return {
+                status: false,
+                message: 'Số điện thoại không hợp lệ'
+            }
+        }
+    }
+}
+
+
+const validReducer = (value, action) => {
+    if(action.type === "VALID") {
+
+        if(action.options.length) {
+            for(let key of action.options) {
+                let inforValid = valid(key, value);
+    
+                if(!inforValid.status) {
+                    return inforValid;
+                }
+            }
+        }
+        
+        return {status: true, message: ''};
+
+    } else {
+        return {status: true, message: ''};
+    }
+}
+
+const useValidator = (options = []) => {
+
+    const blur = (event) => {
+        let value = event.target.value;
+        return validReducer(value, {type: options.length > 0? 'VALID' : 'INVALID', options});
+    }
+
+    return blur;
+}
+
+export default useValidator;
